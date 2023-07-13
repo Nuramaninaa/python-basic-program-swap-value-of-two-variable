780e7a3d1b5d1cf189c3a08a52f542a1e2e80c53
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-13 20:01:09 +0800

    Add files via upload

---
+++ swap-two-variable.py
@@ -0,0 +1,12 @@
+#python program to swap two variable
+
+#initialize value of a and b
+a = int(input ("Enter value of a : "))
+b = int(input ("Enter value of b : "))
+
+#swap the values of two variable
+a,b = b,a
+
+#print value of swap
+print("The value of a after swap is ", a)
+print("The value of b after swap is ", b)
